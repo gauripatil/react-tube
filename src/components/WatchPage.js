26146a2fe71eb5b2625ca7e6f18fc1c28c0b0661
Gauri Patil <gauri.be0811@gmail.com>
2026-03-25 02:33:27 +0530

    Add video data fetching to WatchPage and define YOUTUBE_VIDEO_DETAILS constant

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,17 +1,48 @@
-import { useParams, useSearchParams } from "react-router";
-import { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "./../stores/appSlice";
-
+import { YOUTUBE_VIDEO_DETAILS } from "./../utils/constants";
+let isApiCallInProgress = false;
 const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+  const [videoData, setVideoData] = useState({});
+
   const dispatch = useDispatch();
+  const videoId = searchParams.get("v");
+
   useEffect(() => {
     dispatch(closeMenu());
-  }, []);
 
-  const params = useSearchParams();
-  console.log(params.v);
-  return <div className="col-span-11 p-5 shadow-md">WatchPage</div>;
+    if (!isApiCallInProgress) {
+      getData();
+    }
+    // if (!videoId) return;
+  }, [searchParams]);
+
+  async function getData() {
+    isApiCallInProgress = true;
+    const url = YOUTUBE_VIDEO_DETAILS + "&id=" + videoId;
+    const data = await fetch(url);
+    let vData = await data.json();
+    setVideoData(vData.items[0]);
+    isApiCallInProgress = false;
+  }
+
+  return (
+    <div className="col-span-11 p-5 shadow-md">
+      <iframe
+        width="1050"
+        height="600"
+        src={"https://www.youtube.com/embed/" + videoId}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      <h1 className="font-bold text-lg my-2">{videoData?.snippet?.title}</h1>
+    </div>
+  );
 };
 
 export default WatchPage;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,6 +12,10 @@ export const YOUTUBE_POPULAR_VIDEOS =
   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
+export const YOUTUBE_VIDEO_DETAILS =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  GOOGLE_API_KEY;
+
 export const YT_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyCIg_YzlsrDzYtIu3jXZaEk8jC8LWMLW80";
 
